fb5d676ccb235b1ace278d27c5f9d9c72ebd921e
Ajmerae1234 <2409040@live.stmarys.ac.uk>
2024-11-16 14:14:15 +0000

    Added Frontend Project

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
