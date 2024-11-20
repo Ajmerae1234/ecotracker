b1fe6817eb03e6ef829bef68b734dad5b2b094b7
Ajmerae1234 <2409040@live.stmarys.ac.uk>
2024-11-20 14:37:38 +0000

    Added Logincomponent with state

--- frontend/ecotrack/src/components/LoginComponent.tsx
+++ frontend/ecotrack/src/components/LoginComponent.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     CssBaseline,
     Container,
@@ -10,78 +11,96 @@ import {
 } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 const LoginComponent = () => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const handleEmailChange = (event: any) => {
+        const value = event.target.value;
+        setEmail(value);
+    }
+    const handlePasswordChange = (event: any) => {
+        const value = event.target.value;
+        setPassword(value);
+    }
 
-    return (
-        <Container component="main" maxWidth="xs">
-
-            <Paper
-                elevation={6}
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    padding: 4,
-                    borderRadius: 2
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-
-                <Typography component="h1" variant="h5">
-                    Sign In
-                </Typography>
-                <Box
-                    component="form"
-                    onSubmit={() => { }}
-                    sx={{ mt: 1, width: '100%' }}
-                >
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        value=""
-                        onChange={() => { }}
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value=""
-                        onChange={() => { }}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign In
-                    </Button>
-                    <Button
-                        fullWidth
-                        variant="outlined"
-                        color="secondary"
-                        onClick={() => { }}
-                        sx={{ mt: 1 }}
-                    >
-                        Reset Password
-                    </Button>
-                </Box>
-
-            </Paper>
-        </Container>
-    )
-};
-export default LoginComponent
+    const handleSubmit = () => {
+        console.log("submit", email, password)
+      }
+    
+      
+      const handleClear = () => {
+        setEmail('');
+        setPassword('');
+     }
+     return (
+             <Container component="main" maxWidth="xs">
+     
+                 <Paper
+                     elevation={6}
+                     sx={{
+                         marginTop: 8,
+                         display: 'flex',
+                         flexDirection: 'column',
+                         alignItems: 'center',
+                         padding: 4,
+                         borderRadius: 2
+                     }}
+                 >
+                     <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                         <LockOutlinedIcon />
+                     </Avatar>
+     
+                     <Typography component="h1" variant="h5">
+                         Sign In
+                     </Typography>
+                     <Box
+                         sx={{ mt: 1, width: '100%' }}
+                     >
+                         <TextField
+                             margin="normal"
+                             required
+                             fullWidth
+                             id="email"
+                             label="Email Address"
+                             onChange={handleEmailChange}
+                             name="email"
+                             autoComplete="email"
+                             autoFocus
+                             value={email}
+                         />
+                         <TextField
+                             margin="normal"
+                             required
+                             fullWidth
+                             onChange={handlePasswordChange}
+                             name="password"
+                             label="Password"
+                             type="password"
+                             id="password"
+                             autoComplete="current-password"
+                             value={password}
+                         />
+                         <Button
+                             type="submit"
+                             fullWidth
+                             onClick={handleSubmit}
+                             variant="contained"
+                             sx={{ mt: 3, mb: 2 }}
+                         >
+                             Sign In
+                         </Button>
+                         <Button
+                             fullWidth
+                             variant="outlined"
+                             color="secondary"
+                             onClick={handleClear}
+                             sx={{ mt: 1 }}
+                         >
+                             Reset Password
+                         </Button>
+                     </Box>
+     
+                 </Paper>
+             </Container>
+         )
+     };
+     export default LoginComponent
