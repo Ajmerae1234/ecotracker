8563cfe815421afbcbc78a36152f6c58087ef83f
Ajmerae1234 <2409040@live.stmarys.ac.uk>
2024-12-01 08:18:12 +0000

    added dashboard

--- frontend/ecotrack/src/components/Dashboard.tsx
+++ frontend/ecotrack/src/components/Dashboard.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { 
+import {
     Box,
     AppBar,
     Toolbar,
     Typography,
-    Drawer 
+    Drawer,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemIcon
   } from '@mui/material';
+  import {
+    Home as HomeIcon,
+    DevicesOther as DevicesIcon
+  } from '@mui/icons-material';
+  import Device from './Device'
 const drawerWidth = 240;
 const Dashboard = () => {
+    const [selectedMenu, setSelectedMenu] = React.useState('home');
+     const handleMenuItemClick = (item: any) => {
+    setSelectedMenu(item);
+  };
 return (
 <Box sx={{ display: 'flex' }}>
-<AppBar 
-        position="fixed" 
-        sx={{ 
+<AppBar
+        position="fixed"
+        sx={{
           zIndex: (theme) => theme.zIndex.drawer + 1,
           backgroundColor: '#2196f3' // Customizable app bar color
         }}
@@ -28,17 +41,65 @@ return (
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { 
-            width: drawerWidth, 
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
             boxSizing: 'border-box',
             top: 64, // Adjust based on AppBar height
             height: 'calc(100% - 64px)'
           },
         }}
       >
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            <ListItem
+              component={"button"}
+     
+              onClick={() => handleMenuItemClick('home')}
+             
+            >
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+           
+            <ListItem
+              component={"button"}
+              
+              onClick={() => handleMenuItemClick('devices')}
+             
+             
+            >
+              <ListItemIcon>
+                <DevicesIcon />
+              </ListItemIcon>
+              <ListItemText primary="Devices" />
+            </ListItem>
+          </List>
+        </Box>
 
 </Drawer>
+<Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          marginTop: '64px',
+          marginLeft: `${drawerWidth}px`
+        }}
+      >
+{selectedMenu === 'home' && (
+          <Typography paragraph>
+            Home Content
+          </Typography>
+        )}
+        {selectedMenu === 'devices' && (
+          <Typography paragraph>
+            Devices Content
+          </Typography>
+        )}
 </Box>
-) 
+</Box>
+)
 }
 export default Dashboard;
---
+++ frontend/ecotrack/src/components/Device.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Device = () => {
+  return(<div>
+    Hello Devices
+  </div>)
+
+}
+
+export default Device
